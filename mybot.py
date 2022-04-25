fd70ff86c87a782ea1f88b474ab860e8e3073425
alex <alex@mail.ru>
2022-04-25 18:38:02 +0300

    first commit

---
+++ mybot.py
@@ -0,0 +1,21 @@
+import telebot
+from telebot import types
+
+token = '5105367052:AAF_6OpsDUKr0nWBE27aF1qSrG98RZS6aHc'
+bot = telebot.TeleBot(token)
+
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, "Привет ✌ ")
+
+
+@bot.message_handler(commands=['button'])
+def button_message(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    item1 = types.KeyboardButton("Кнопка")
+    markup.add(item1)
+    bot.send_message(message.chat.id, 'Выберите что вам надо', reply_markup=markup)
+
+
+bot.infinity_polling()
